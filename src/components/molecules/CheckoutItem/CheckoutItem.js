175bdf4702242328d1e36e09793f6f3b237b24a4
Maciej <hydroggen@o2.pl>
2021-11-29 21:46:05 +0100

    move checkout item styles to other file

--- src/components/molecules/CheckoutItem/CheckoutItem.js
+++ src/components/molecules/CheckoutItem/CheckoutItem.js
@@ -1,58 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Button } from 'components/atoms/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const StyledItem = styled.div`
-  width: 100%;
-  min-height: 10rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-right: 5rem;
-
-  p {
-    width: 23%;
-    text-align: center;
-    font-size: ${({ theme }) => theme.fontSize.m};
-    margin: 0 1rem;
-  }
-
-  span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 23%;
-
-    div {
-      font-size: 1.5rem;
-      cursor: pointer;
-    }
-  }
-`;
-
-const ImageWrapper = styled.div`
-  width: 23%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 3.5rem;
-  padding: 1rem 0;
-
-  img {
-    width: 8rem;
-    border-radius: 0.8rem;
-    box-shadow: 0 0.3rem 0.6rem rgba(0, 0, 0, 0.16),
-      0 0.3rem 0.6rem rgba(0, 0, 0, 0.23);
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  width: 23%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { ButtonWrapper, ImageWrapper, StyledItem } from './CheckoutItem.styles';
 
 const CheckoutItem = ({
   itemData: { imageUrl, name, quantity, price },
