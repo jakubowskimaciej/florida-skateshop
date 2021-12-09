16f86d4b5f17ed1eba8f81f16dbe65ea6633db84
Maciej <hydroggen@o2.pl>
2021-12-09 21:27:33 +0100

    test

--- src/components/molecules/Navigation/Navigation.js
+++ src/components/molecules/Navigation/Navigation.js
@@ -15,7 +15,7 @@ const Navigation = ({ history }) => {
   return (
     <Wrapper>
       <h1 onClick={() => history.push(`/`)}>
-        <span>florida</span> deploy test
+        <span>florida</span> skateshop
       </h1>
       <LinkWrapper>
         <StyledLink to={'/shop'}>shop</StyledLink>
